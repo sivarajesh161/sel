7a08f37438cbb54000df11b603c36dee71be1e7f
siva rajesh <sivarajesh161@gmail.com>
2024-01-02 15:33:26 +0530

    Progrm2.java is modified on branch1 03:32pm 02/01/23

--- Progrm2.java
+++ Progrm2.java
@@ -1,7 +1,7 @@
 package com.sel;
 
 public class Progrm2 {
-	///modified file in Git Hub on master branch 
+	///modified file in Git Hub on mister branch 
 
 	public static void main(String[] args) 
 	{
